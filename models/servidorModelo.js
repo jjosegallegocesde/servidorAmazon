148fd34f800aef5b3cbb6f5c922dc7d4deb571ef
jjosegallegocesde <61057518+jjosegallegocesde@users.noreply.github.com>
2021-09-22 16:51:53 -0500

    post

--- models/servidorModelo.js
+++ models/servidorModelo.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { conectarBD } = require('../database/conexion.js')
+const FacturaModelo = require("../models/facturaModelo.js")
 
 class SevidorModelo {
 
@@ -8,6 +9,7 @@ class SevidorModelo {
 
         this.app = express();
         this.levantarBD();
+        this.auxiliares();
         this.llamarRutas();
 
 
@@ -25,14 +27,42 @@ class SevidorModelo {
         conectarBD();
     }
 
+    auxiliares(){
+        this.app.use(express.json())
+    }
+
+
     llamarRutas() {
 
         this.app.get('/facturas', function (req, res) {
             res.send('Buenas tardes Juan Jose');
         });
 
-        this.app.post('/facturas/nuevo', function (req, res) {
-            res.send('Buenas tardes Juan Jose');
+        this.app.post('/facturas/nuevo', async function (req, res) {
+
+            let datosFactura=req.body;
+            
+            try{
+
+                let factura=new FacturaModelo(datosFactura);
+                await factura.save();
+                res.status(200).json({
+                    respuesta:"exito",
+                    datos:factura
+                })
+
+
+
+            }catch(error){
+
+                res.status(400).json({
+                    respuesta:"error",
+                    datos:error
+                })
+
+
+            }
+            
         });
 
         this.app.put('/facturas/cambiar', function (req, res) {
